a53ffaf436b6e355bc6ac16f841a0079465066d0
Abhi Dhadke <abhidhadke@gmail.com>
2024-02-11 21:10:17 +0530

    First Commit

---
+++ Components/Footer.jsx
@@ -0,0 +1,35 @@
+import { Fot1, Fot2 } from "../Components/index";
+
+export default () => {
+  const footerNavs = [
+    {
+      href: "javascript:void()",
+      name: "Terms",
+    },
+    {
+      href: "javascript:void()",
+      name: "License",
+    },
+    {
+      href: "javascript:void()",
+      name: "Privacy",
+    },
+    {
+      href: "javascript:void()",
+      name: "About us",
+    },
+  ];
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="justify-between sm:flex">
+          
+          
+        </div>
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© BlockBasket. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
